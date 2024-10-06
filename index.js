eb80961ae4e648535ed5c5405ac872400b307fc0
Suraj <Surajsinghnagrkoti2992003@gmail.com>
2024-10-06 11:57:49 +0530

    add the code to find odd numbers

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let arr=[1,2,3,4,5,6,7,8,9]
 let even="";
+let odd="";
 for(let i=0;i<arr.length;i++){
     if(arr[i] % 2 == 0){
         even = even + arr[i]
-        
+        console.log("Even Num:",even)
     }
-    console.log(even)
+    else if(arr[i] % 2 == 1){
+        odd = odd + arr[i]
+        console.log("Odd Num:",odd)
+    }
 }
 
 
